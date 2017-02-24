a2cc20da4286cfe00a8c01325e8c03c136806068
Tamal H <tamalnh@users.noreply.github.com>
2017-02-24 14:43:36 +0600

    Add files via upload

---
+++ iterating_array.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+    var newArray = ['zero', 'one', 'two', 'three'];
+    newArray.forEach(function (value, index) {
+        console.log('this is the value:' + value + '. Annd this is the index number:' + index);
+    });
+
+    console.log(newArray.every(function(value){
+        return typeof value !== 'string';
+    }));// if the values of array type all are string the it will return Ture other wise will return false.
+    
+    
+    var mixArray = [1, 'two', 3, 'four', 5, 'six'];
+    console.log(mixArray.filter(function(value){
+        return typeof value == 'number';
+    }));
+    
+    
+    console.log(newArray.map(function(value){
+        return value.toUpperCase();//map just make an new array and then convert all value ToUpperCase
+    }));
+}());
